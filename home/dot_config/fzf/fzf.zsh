3c785ab22cff757f4936f516513570082b628bb5
madeth <madeth@users.noreply.github.com>
2026-01-29 22:10:24 +0900

    Update home/.config/fzf/fzf.zsh

--- home/dot_config/fzf/fzf.zsh
+++ home/dot_config/fzf/fzf.zsh
@@ -1,11 +1,18 @@
 if (( $+commands[fzf] )); then
-  zsh-defer source <(fzf --zsh)
-  export FZF_DEFAULT_OPTS='
-    --height 80%
-    --reverse
-    --border
-  '
-  export FZF_DEFAULT_COMMAND='fd --type f'
-  export FZF_CTRL_T_COMMAND='fd --type f'
-  export FZF_ALT_C_COMMAND='fd --type d'
+  zsh-defer fzf-reload(){
+    echo "hoge"
+    source <(fzf --zsh)
+    export FZF_DEFAULT_OPTS='
+      --height 80%
+      --reverse
+      --border
+    '
+    export FZF_DEFAULT_COMMAND='fd --type f'
+    export FZF_CTRL_T_COMMAND='fd --type f'
+    export FZF_ALT_C_COMMAND='fd --type d'
+
+    bindkey -r '^T'
+    bindkey '^F' fzf-file-widget
+  }
+  fzf-reload
 fi
